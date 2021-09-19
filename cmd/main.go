98a04698ce613fdcb0322e3667baa127618309df
Error996 <12880110+Error996@users.noreply.github.com>
2021-09-19 21:38:56 +0800

    Create main.go

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"github.com/Error996/udp2tcp"
+	"time"
+)
+
+var config struct {
+	Client string
+	PointA string
+	Server string
+	PointZ string
+}
+
+func main() {
+	flag.StringVar(&config.Client, "c", "", "client listen address")
+	flag.StringVar(&config.PointA, "a", "", "A point")
+	flag.StringVar(&config.Server, "s", "", "server listen address")
+	flag.StringVar(&config.PointZ, "z", "", "Z point")
+	flag.Parse()
+
+	if config.Client != "" {
+		if config.PointA != "" {
+			udp2tcp_client.StartClient(config.Client, config.PointA, time.Minute * 3)
+		}
+	}
+
+	if config.Server != "" {
+		if config.PointZ != "" {
+			udp2tcp_server.StartServer(config.Server, config.PointZ, time.Minute * 3)
+		}
+	}
+}
